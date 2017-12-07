7c411fb70c2c06f3b2c4dae4660722b9ffd3de21
seenfan <seen.ys.fan@mail.foxconn.com>
2017-12-07 09:19:54 +0800

    Add messges output for restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo "restarting services..."
+echo "restart openstack-cinder-volume"
 systemctl restart openstack-cinder-volume
+echo "restart openstack-cinder-backup"
 systemctl restart openstack-cinder-backup
+echo "restart openstack-glance-api"
 systemctl restart openstack-glance-api
+echo "restart openstack-nova-compute"
 systemctl restart openstack-nova-compute
